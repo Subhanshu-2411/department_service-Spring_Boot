74710a42175ce1264916f798eacdf25b6381555a
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-14 15:00:44 +0530

    Added Files and Folders

---
+++ src/main/java/com/example/departmentservice/entity/Department.java
@@ -0,0 +1,23 @@
+package com.example.departmentservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
